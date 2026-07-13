788c0a6c1437bf125eb45f05c61b75c8fd171714
Markus4771 <162835583+Markus4771@users.noreply.github.com>
2026-07-13 16:50:16 +0200

    Add 3CX V20 API constants

--- custom_components/threecx/const.py
+++ custom_components/threecx/const.py
@@ -7,6 +7,8 @@ PLATFORMS = ["sensor", "binary_sensor"]
 
 CONF_VERIFY_SSL = "verify_ssl"
 CONF_API_MODE = "api_mode"
+CONF_CLIENT_ID = "client_id"
+CONF_CLIENT_SECRET = "client_secret"
 
 DEFAULT_NAME = "3CX"
 DEFAULT_PORT = 443
@@ -16,3 +18,8 @@ DEFAULT_SCAN_INTERVAL = timedelta(seconds=30)
 API_MODE_AUTO = "auto"
 API_MODE_V20 = "v20"
 API_MODE_LEGACY = "legacy"
+
+TOKEN_PATH = "/connect/token"
+XAPI_BASE_PATH = "/xapi/v1"
+XAPI_DEFS_PATH = f"{XAPI_BASE_PATH}/Defs?$select=Id"
+XAPI_USERS_PATH = f"{XAPI_BASE_PATH}/Users?$select=Id,Number,FirstName,LastName"
